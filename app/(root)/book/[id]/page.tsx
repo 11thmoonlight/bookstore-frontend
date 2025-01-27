f2a07984f1bd349e2e92844b5a30933382eea38f
Aida <118523650+11thmoonlight@users.noreply.github.com>
2025-01-28 00:09:48 +0330

    re design id book page and make it responsive

--- app/(root)/book/[id]/page.tsx
+++ app/(root)/book/[id]/page.tsx
@@ -24,36 +24,43 @@ export default function BooksById() {
   }
   return (
     <>
-      <div className="flex gap-10 mt-[160px] mb-10 justify-center px-10">
-        <div className="relative">
+      <div className="flex flex-col md:flex-col lg:flex-row gap-10 mt-[160px] mb-10 justify-center px-10 lg:h-[370px]">
+        <div className="flex flex-col md:flex-row gap-10">
           <Image
             src={`http://localhost:1337/${book?.image[0]?.url}`}
             alt="book image cover"
-            width={400}
-            height={700}
+            width={430}
+            height={900}
+            className="rounded-xl shadow-lg"
           />
-        </div>
-        <div className="w-fit flex flex-col gap-3">
-          <h2 className="font-bold text-2xl">{book?.name}</h2>
-          <div className="text-lg flex gap-4 items-center">
-            <Avatar>
-              <AvatarImage
-                src={`http://localhost:1337/${book?.authorImg.url}`}
-                alt="writer image"
-              />
-              <AvatarFallback>A</AvatarFallback>
-            </Avatar>
-            <p>By {book?.author}</p>
-          </div>
-          <div className="flex items-center gap-2">
-            <StarRating rating={book?.rate ?? 0} />
-            <p>{book?.rate}</p>
-          </div>
 
-          <div className="text-sm">{book?.description}</div>
+          <div className="w-fit flex flex-col gap-3 px-6 py-2 shadow-transparent bg-amber-50 rounded-xl md:h-[500px] lg:h-[370px]">
+            <h2 className="font-bold text-2xl text-amber-950">{book?.name}</h2>
+            <div className="text-lg flex gap-4 items-center">
+              <Avatar>
+                <AvatarImage
+                  src={`http://localhost:1337/${book?.authorImg.url}`}
+                  alt="writer image"
+                />
+                <AvatarFallback>{book?.author}</AvatarFallback>
+              </Avatar>
+              <p className="text-amber-950">By {book?.author}</p>
+            </div>
+            <div className="flex items-center gap-2">
+              <StarRating rating={book?.rate ?? 0} />
+              <p className="text-amber-950">{book?.rate}</p>
+            </div>
+
+            <div
+              className="text-sm overflow-y-auto text-amber-950"
+              style={{ scrollbarWidth: "none" }}
+            >
+              {book?.description}
+            </div>
+          </div>
         </div>
 
-        <Card className="bg-amber-50 w-full md:w-[700px] h-fit">
+        <Card className="bg-amber-50 w-full md:w-[700px] h-full">
           <CardHeader>
             <CardTitle className="pb-4 text-amber-800 border-b-2">
               Details
@@ -86,6 +93,15 @@ export default function BooksById() {
               <p className="font-bold text-amber-800">Genere</p>
               <p className="text-amber-800">{book?.category}</p>
             </div>
+
+            {book?.discount && book?.discount > 0 && (
+              <div className="flex justify-between">
+                <p className="font-bold text-amber-800">Discount</p>
+                <p className="text-red-700 text-lg font-bold">
+                  {book?.discount}$
+                </p>
+              </div>
+            )}
 
             <div className="flex justify-between">
               <p className="font-bold text-amber-800">Price</p>
