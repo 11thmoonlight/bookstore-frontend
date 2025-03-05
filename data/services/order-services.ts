f0de1b777303330437a4383e9a456c1dcf58880c
aida <aidasaeedi1998@gmail.com>
2025-03-05 22:59:58 +0330

    change current order filter and add delivered order service

--- data/services/order-services.ts
+++ data/services/order-services.ts
@@ -1,23 +1,6 @@
 import axiosInstance from "@/lib/axiosInstance";
 import qs from "qs";
 
-// Query for populating necessary relations
-const query = qs.stringify({
-  populate: {
-    cart_items: {
-      populate: {
-        product: {
-          populate: {
-            image: {
-              fields: ["url", "alternativeText"],
-            },
-          },
-        },
-      },
-    },
-  },
-});
-
 // create new order
 export async function createOrder(
   orderData: OrderProps,
@@ -44,17 +27,73 @@ export async function createOrder(
 }
 
 // get order
-export async function getOrder(orderId: string) {
-  try {
-    const response = await axiosInstance.get(
-      `/api/orders/${orderId}?${query}`,
-      {}
-    );
+export async function getOrder(userId: string) {
+  const query = qs.stringify({
+    filters: {
+      orderStatus: {
+        $in: ["order placed", "processing", "shipped"],
+      },
+      users_permissions_user: userId,
+    },
+    populate: {
+      cart_items: {
+        populate: {
+          product: {
+            populate: {
+              image: {
+                fields: ["url", "alternativeText"],
+              },
+            },
+          },
+        },
+      },
+    },
+  });
 
+  try {
+    const response = await axiosInstance.get(`/api/orders?${query}`);
     return { ok: true, data: response.data, error: null };
   } catch (error: unknown) {
     if (error instanceof Error) {
-      console.error("Error fetching order:", error.message);
+      console.error("Error fetching filtered orders:", error.message);
+      return { ok: false, data: null, error };
+    } else {
+      console.error("An unexpected error occurred:", error);
+      return { ok: false, data: null, error: new Error("Unknown error") };
+    }
+  }
+}
+
+// get delivered orders
+export async function getDeliveredOrders(userId: string) {
+  const query = qs.stringify({
+    filters: {
+      orderStatus: {
+        $eq: "delivered",
+      },
+      users_permissions_user: userId,
+    },
+    populate: {
+      cart_items: {
+        populate: {
+          product: {
+            populate: {
+              image: {
+                fields: ["url", "alternativeText"],
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+
+  try {
+    const response = await axiosInstance.get(`/api/orders?${query}`);
+    return { ok: true, data: response.data, error: null };
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error("Error fetching delivered orders:", error.message);
       return { ok: false, data: null, error };
     } else {
       console.error("An unexpected error occurred:", error);
