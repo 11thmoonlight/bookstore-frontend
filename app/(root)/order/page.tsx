7df1f4a6524b4477c2c49e9c0fc94a377130b223
aida <aidasaeedi1998@gmail.com>
2025-03-05 22:59:09 +0330

    change current order function place

--- app/(root)/order/page.tsx
+++ app/(root)/order/page.tsx
@@ -2,34 +2,10 @@
 
 import React from "react";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { useFetchOrder } from "@/hooks/useOrder";
-import { useUser } from "@/context/userContext";
-import { formatDate } from "@/lib/utils";
 import CurrentOrderTab from "@/components/CurrentOrder";
 import DeliveredOrderTab from "@/components/DeliveredOrder";
-import { stages } from "@/components/OrderProcessingChart";
-import Loader from "@/components/custom/Loader";
-import ErrorMessage from "@/components/custom/ErrorMessage";
 
 const Order: React.FC = () => {
-  const { user } = useUser();
-
-  const { order, loading, error } = useFetchOrder(
-    user?.orders[0]?.documentId || ""
-  );
-
-  const formattedDate = formatDate(order?.createdAt ?? "");
-
-  const currentStatus = order?.orderStatus || "order placed";
-  const currentStage = stages.findIndex(
-    (stage) => stage.status === currentStatus
-  );
-
-  console.log(order);
-
-  if (loading) return <Loader />;
-  if (error) return <ErrorMessage />;
-
   return (
     <div className="mt-[160px] md:px-20 px-2 md:flex md:flex-row flex flex-col gap-4 mb-6 justify-center items-start text-sm md:text-base">
       <Tabs defaultValue="current" className="w-full">
@@ -38,13 +14,7 @@ const Order: React.FC = () => {
           <TabsTrigger value="delivered">Delivered Order</TabsTrigger>
         </TabsList>
 
-        {order && (
-          <CurrentOrderTab
-            order={order}
-            currentStage={currentStage}
-            formattedDate={formattedDate}
-          />
-        )}
+        <CurrentOrderTab />
         <DeliveredOrderTab />
       </Tabs>
     </div>
