d4d1855df5345233e765c56df6de3fdecaa469f8
aida <aidasaeedi1998@gmail.com>
2025-03-13 21:29:22 +0330

    fix checkout service error

--- data/services/createCheckout.ts
+++ data/services/createCheckout.ts
@@ -1,7 +1,9 @@
 import { loadStripe } from "@stripe/stripe-js";
+
 const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLICATION_KEY!
 );
+
 export async function createCheckout(total: number) {
   try {
     const amountInCents = Math.round(total * 100);
@@ -17,13 +19,20 @@ export async function createCheckout(total: number) {
         }),
       }
     );
+
     const { id: sessionId } = await res.json();
     const stripe = await stripePromise;
-    const { error } = await stripe?.redirectToCheckout({
-      sessionId,
-    });
-    if (error) {
-      console.error(error.message);
+
+    if (stripe) {
+      const { error } = await stripe.redirectToCheckout({
+        sessionId,
+      });
+
+      if (error) {
+        console.error(error.message);
+      }
+    } else {
+      console.error("Stripe has not loaded correctly");
     }
   } catch (err) {
     console.error("Error in Checkout:", err);
