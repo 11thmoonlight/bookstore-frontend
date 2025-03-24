379d11a1fa1f1e91d0b181b9cbaf9bc850f13748
aida <aidasaeedi1998@gmail.com>
2025-03-24 21:59:57 +0330

    fixing login state

--- data/services/get-token.ts
+++ data/services/get-token.ts
@@ -1,10 +1,10 @@
 import { cookies } from "next/headers";
 
 export async function getAuthToken() {
-  let authToken = cookies().get("jwt")?.value;
+  let authToken: string | undefined = cookies().get("jwt")?.value;
 
   if (!authToken) {
-    authToken = localStorage.getItem("jwt");
+    authToken = localStorage.getItem("jwt") ?? undefined;
   }
 
   return authToken;
