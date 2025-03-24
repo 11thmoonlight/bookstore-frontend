4ff7c46cd07032fde5291f6514f24b48d3ddf30c
aida <aidasaeedi1998@gmail.com>
2025-03-24 23:07:50 +0330

    error

--- data/services/get-token.ts
+++ data/services/get-token.ts
@@ -1,11 +1,6 @@
 import { cookies } from "next/headers";
 
 export async function getAuthToken() {
-  // let authToken: string | undefined = cookies().get("jwt")?.value;
-
-  // if (!authToken) {
-  //   authToken = localStorage.getItem("jwt") ?? undefined;
-  // }
   const authToken = cookies().get("jwt")?.value;
 
   return authToken;
