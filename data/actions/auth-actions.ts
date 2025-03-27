ce89e9570bd2d5991dc2b2565765fd1fb088df13
aida <aidasaeedi1998@gmail.com>
2025-03-27 19:56:35 +0330

    fix config

--- data/actions/auth-actions.ts
+++ data/actions/auth-actions.ts
@@ -11,7 +11,7 @@ import {
 const config = {
   maxAge: 60 * 60 * 24 * 7, // 1 week
   path: "/",
-  domain: "https://backend-production-dd5c.up.railway.app",
+  domain: "backend-production-dd5c.up.railway.app",
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
 };
